9bb225d2cf42bee8f503074e8a007419db2932a2
MamunHossain005 <hossainmamun624@gmail.com>
2025-05-07 03:02:06 +0600

    final commit 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = {
   ...compat.config({
-    extends: ['next/core-web-vitals', 'prettier', 'next/typescript', 'plugin:tailwindcss/recommended', 'plugin:@tailwindcss/recommended'],
-    plugins: ['prettier', '@tailwindcss'],
+    extends: [
+      'next/core-web-vitals', 
+      'plugin:prettier/recommended',  // Changed from 'prettier'
+      'plugin:@typescript-eslint/recommended',
+      'plugin:tailwindcss/recommended'
+    ],
+    plugins: ['prettier', 'tailwindcss'],
     rules: {
       'prettier/prettier': 'error',
       'no-console': 'warn',
       'no-unused-vars': 'error',
       'react/no-escape-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
     },
   }),
 };
